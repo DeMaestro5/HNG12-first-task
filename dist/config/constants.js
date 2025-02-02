08133dc2f1ae61626b45ad4cd6d4c28f42870b61
stephen <osstephen70@gmail.com>
2025-02-02 11:32:09 +0100

    fix

--- dist/config/constants.js
+++ dist/config/constants.js
@@ -1,8 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CONFIG = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 exports.CONFIG = {
-    EMAIL: 'osstephen70@gmail.com',
-    GITHUB_URL: 'https://github.com/DeMaestro5/HNG12-first-task',
+    EMAIL: process.env.EMAIL,
+    GITHUB_URL: process.env.GITHUB_URL,
     PORT: process.env.PORT || 4001,
 };
